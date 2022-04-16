c84fb65d0b00c2cca8e24f264da38e6b0d8422eb
Culprit <kylebrock@gmail.com>
2022-04-16 16:04:15 -0600

    modified settings

--- settings.py
+++ settings.py
@@ -1,12 +1,10 @@
 class Settings:
     '''A class to store all game settings.'''
 
-    def __init__(self):
-        '''Init game settings.'''
-        # Screen settings.
-        self.screen_width = 800
-        self.screen_height = 800
-        self.bg_color = (20, 10, 70)
+    # Screen settings.
+    screen_width = 800
+    screen_height = 800
+    bg_color = (20, 10, 70)
 
-        # Ship settings.
-        self.ship_speed = 0.4
+    # Ship settings.
+    ship_speed = 0.4
